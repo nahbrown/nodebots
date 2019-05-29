bfefde936cd44ab465173b7d0487b7686ab2acc0
2 <a2@HS-Lib-Lab-23.local>
2019-05-29 09:26:36 -0700

    added a single colon

--- j5webserver.js
+++ j5webserver.js
@@ -25,7 +25,7 @@ var board = new five.Board();
   io.sockets.on('connection', function (socket) {// WebSocket Connection
     console.log("Connected to Websocket on port 8080");
     socket.on('light', function(data) { //get light switch status from client
-      console.log("Switch has been switched" + data);
+      console.log("Switch has been switched: " + data);
       led.toggle();
 
     });
